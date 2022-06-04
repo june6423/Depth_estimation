2c4fd98839b977632c2f65f29a69952155ef16d2
june6423 <june6423@naver.com>
2022-06-05 00:16:38 +0900

    00

---
+++ Depth_estimation/pytorch/std_scripts/train_dfd.sh
@@ -0,0 +1,36 @@
+# Train on DFD indoor dataset
+
+# Standard
+export CUDA_VISIBLE_DEVICES=0
+port=8001
+display_id=100
+
+# Training
+NEPOCHS=100
+
+weightDecay=1e-4
+
+# data augmentation: hflip vfkip scale color rotate 
+
+NAME="dfd_train" # Change the name when training a new project
+DATAROOT="../dfd_datasets/zed_dataset/"
+BATCHSIZE=4
+MTL_MODEL="eweights" # This project is adaptable for single and multitask objectives
+MODEL="monotask"
+
+TASKS='depth'
+OUTPUTS_NC='1'
+
+NET_ARCHITECTURE="D3net_multitask"
+
+# Visualization
+DISPLAY_ID=1
+PORT=8001
+IMAGE_SIZE=256
+SCALE=1000.0
+MAX_D=10.0
+DISPLAY_FREQ=50
+
+SAVE_CKPT_FREQ=100
+
+python ./main.py --name $NAME --dataroot $DATAROOT --train --batchSize $BATCHSIZE --model $MODEL --mtl_method $MTL_MODEL --port $PORT --display_id $DISPLAY_ID --net_architecture $NET_ARCHITECTURE --pretrained --use_crop --use_padding --data_augmentation t f f f t --max_distance $MAX_D --scale_to_mm $SCALE --train --tasks $TASKS --outputs_nc $OUTPUTS_NC --use_skips --display_freq $DISPLAY_FREQ --lr 2e-4 --optim Adam --weightDecay $weightDecay --nThreads 1 --imageSize $IMAGE_SIZE --nEpochs $NEPOCHS --save_checkpoint_freq $SAVE_CKPT_FREQ
